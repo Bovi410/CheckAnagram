da02f39749e727a925e20cfb016c7f5aa02a7d47
Bovi410 <50920315+Bovi410@users.noreply.github.com>
2020-06-23 15:03:18 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;  
+namespace Anagram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first word:");
+            string word1 = Console.ReadLine();
+            Console.Write("Enter second word:");
+            string word2 = Console.ReadLine();
+
+            char[] ch1 = word1.ToLower().ToCharArray();
+            char[] ch2 = word2.ToLower().ToCharArray();
+
+            Array.Sort(ch1);
+            Array.Sort(ch2);
+
+            string val1 = new string(ch1);
+            string val2 = new string(ch2);
+
+            if (val1 == val2)
+            {
+                Console.WriteLine("These words are anagrams");
+            }
+            else
+            {
+                Console.WriteLine("These words arent anagrams");
+            }
+
+        }
+    }
+}
